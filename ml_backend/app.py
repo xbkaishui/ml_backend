4940d1776ba9e2eb3e4d4600b39692da509ccf1a
xbkaishui <xbkaishui@users.noreply.github.com>
2023-06-23 13:45:20 +0000

    ✅ Ready to clone and code.

--- ml_backend/app.py
+++ ml_backend/app.py
@@ -23,18 +23,18 @@ def read(*paths, **kwargs):
 
 
 description = """
-project_name API helps you do awesome stuff. 🚀
+ml_backend API helps you do awesome stuff. 🚀
 """
 
 app = FastAPI(
-    title="project_name",
+    title="ml_backend",
     description=description,
     version=read("VERSION"),
-    terms_of_service="http://project_name.com/terms/",
+    terms_of_service="http://ml_backend.com/terms/",
     contact={
-        "name": "author_name",
-        "url": "http://project_name.com/contact/",
-        "email": "author_name@project_name.com",
+        "name": "xbkaishui",
+        "url": "http://ml_backend.com/contact/",
+        "email": "xbkaishui@ml_backend.com",
     },
     license_info={
         "name": "The Unlicense",
--- ml_backend/config.py
+++ ml_backend/config.py
@@ -5,11 +5,11 @@ from dynaconf import Dynaconf
 HERE = os.path.dirname(os.path.abspath(__file__))
 
 settings = Dynaconf(
-    envvar_prefix="project_name",
+    envvar_prefix="ml_backend",
     preload=[os.path.join(HERE, "default.toml")],
     settings_files=["settings.toml", ".secrets.toml"],
     environments=["development", "production", "testing"],
-    env_switcher="project_name_env",
+    env_switcher="ml_backend_env",
     load_dotenv=False,
 )
 
@@ -18,7 +18,7 @@ settings = Dynaconf(
 # How to use this application settings
 
 ```
-from project_name.config import settings
+from ml_backend.config import settings
 ```
 
 ## Acessing variables
@@ -45,15 +45,15 @@ KEY=value
 
 ### As environment variables
 ```
-export PROJECT_NAME_KEY=value
-export PROJECT_NAME_KEY="@int 42"
-export PROJECT_NAME_KEY="@jinja {{ this.db.uri }}"
-export PROJECT_NAME_DB__uri="@jinja {{ this.db.uri | replace('db', 'data') }}"
+export ml_backend_KEY=value
+export ml_backend_KEY="@int 42"
+export ml_backend_KEY="@jinja {{ this.db.uri }}"
+export ml_backend_DB__uri="@jinja {{ this.db.uri | replace('db', 'data') }}"
 ```
 
 ### Switching environments
 ```
-PROJECT_NAME_ENV=production project_name run
+ml_backend_ENV=production ml_backend run
 ```
 
 Read more on https://dynaconf.com
